ecbaa1627167c4e9947f0b4490f115ccbd822e6d
d1snin <me@d1s.dev>
2022-03-27 01:33:45 +0300

    refactored configuration properties

--- spring-boot-starter-lp-server/src/main/kotlin/dev/d1s/lp/server/properties/LongPollingEventServerConfigurationProperties.kt
+++ spring-boot-starter-lp-server/src/main/kotlin/dev/d1s/lp/server/properties/LongPollingEventServerConfigurationProperties.kt
@@ -2,9 +2,11 @@ package dev.d1s.lp.server.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.validation.annotation.Validated
 import java.time.Duration
 import javax.validation.constraints.NotNull
 
+@Validated
 @ConstructorBinding
 @ConfigurationProperties("long-polling-server")
 public data class LongPollingEventServerConfigurationProperties(
