07a0e2cf4472bc5dcaee6da90487f2c400b4c35a
d1snin <me@d1s.dev>
2022-04-16 03:14:42 +0300

    added non-blocking implementation of publisher

---
+++ spring-boot-starter-lp-server/src/main/kotlin/dev/d1s/lp/server/publisher/impl/AsyncLongPollingEventPublisherImpl.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Mikhail Titov and other contributors (if even present)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.d1s.lp.server.publisher.impl
+
+import dev.d1s.lp.commons.domain.LongPollingEvent
+import dev.d1s.lp.server.publisher.AsyncLongPollingEventPublisher
+import dev.d1s.lp.server.service.LongPollingEventService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Async
+import java.time.Instant
+import java.util.concurrent.CompletableFuture
+
+// is open really needed? I'm using a plugin for Kotlin support, but Intellij warns me anyway.
+internal open class AsyncLongPollingEventPublisherImpl : AsyncLongPollingEventPublisher {
+
+    @Autowired
+    private lateinit var longPollingEventService: LongPollingEventService
+
+    @Async
+    override fun <T : Any> publish(group: String, principal: String, data: T): CompletableFuture<LongPollingEvent<T>> {
+        val event = LongPollingEvent(
+            group,
+            Instant.now(),
+            principal,
+            data
+        )
+
+        longPollingEventService.add(event)
+
+        return CompletableFuture.completedFuture(event)
+    }
+}
---
+++ spring-boot-starter-lp-server/src/test/kotlin/dev/d1s/lp/server/publisher/AsyncLongPollingEventPublisherImplTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 Mikhail Titov and other contributors (if even present)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.d1s.lp.server.publisher
+
+import com.ninjasquad.springmockk.MockkBean
+import dev.d1s.lp.commons.test.mockLongPollingEvent
+import dev.d1s.lp.server.publisher.impl.AsyncLongPollingEventPublisherImpl
+import dev.d1s.lp.server.service.LongPollingEventService
+import dev.d1s.teabag.testing.constant.VALID_STUB
+import io.mockk.every
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+@SpringBootTest
+@ContextConfiguration(classes = [AsyncLongPollingEventPublisherImpl::class])
+internal class AsyncLongPollingEventPublisherImplTest {
+
+    @Autowired
+    private lateinit var publisher: AsyncLongPollingEventPublisherImpl
+
+    @MockkBean
+    private lateinit var longPollingEventService: LongPollingEventService
+
+    private val event = mockLongPollingEvent
+
+    @BeforeEach
+    fun setup() {
+        every {
+            longPollingEventService.add(event)
+        } returns setOf()
+    }
+
+    @Test
+    fun `should publish event`() {
+        val event = assertDoesNotThrow {
+            publisher.publish(VALID_STUB, VALID_STUB, VALID_STUB)
+        }.get()
+
+        expectThat(event.group) isEqualTo VALID_STUB
+        expectThat(event.principal) isEqualTo VALID_STUB
+        expectThat(event.data) isEqualTo VALID_STUB
+    }
+}
