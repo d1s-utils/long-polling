c46e3bfe6739affd5ce63fb709861dcb4d930f43
d1snin <me@d1s.dev>
2022-04-22 15:51:29 +0300

    bumped the deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.spring") version "1.6.20" apply false
-    id("org.springframework.boot") version "2.6.4" apply false
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21" apply false
+    id("org.springframework.boot") version "2.6.7" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
 }
 
