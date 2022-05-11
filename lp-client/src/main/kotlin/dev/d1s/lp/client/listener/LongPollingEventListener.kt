70a30a690a3e7e763c0491848af9df43cb30d86a
d1snin <me@d1s.dev>
2022-05-11 18:52:09 +0300

    made listener function suspended

--- lp-client/src/main/kotlin/dev/d1s/lp/client/listener/LongPollingEventListener.kt
+++ lp-client/src/main/kotlin/dev/d1s/lp/client/listener/LongPollingEventListener.kt
@@ -18,4 +18,4 @@ package dev.d1s.lp.client.listener
 
 import dev.d1s.lp.commons.domain.LongPollingEvent
 
-public typealias LongPollingEventListener<T> = LongPollingEvent<T>.() -> Unit
+public typealias LongPollingEventListener<T> = suspend LongPollingEvent<T>.() -> Unit
