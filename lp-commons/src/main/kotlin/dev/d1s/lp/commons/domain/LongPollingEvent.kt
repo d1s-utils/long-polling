7c4dd23f1bcf474c20d697f93b42b8f7fb36e990
d1snin <me@d1s.dev>
2022-05-11 18:44:19 +0300

    made principal nullable

--- lp-commons/src/main/kotlin/dev/d1s/lp/commons/domain/LongPollingEvent.kt
+++ lp-commons/src/main/kotlin/dev/d1s/lp/commons/domain/LongPollingEvent.kt
@@ -21,7 +21,7 @@ import java.time.Instant
 public data class LongPollingEvent<T : Any>(
     val group: String,
     val timestamp: Instant,
-    val principal: String,
+    val principal: String?,
     val data: T
 ) {
 
